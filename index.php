54fdf1f5b1f943fbf1375552b1553f6cc2cba011
dzmitry216 <matlashdzmitry@gmail.com>
2021-07-28 16:31:01 +0300

    check

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <input type="text" name="mail" class="form-control" placeholder="Почта" aria-label="Почта" aria-describedby="basic-addon2">
         <span class="input-group-text" id="basic-addon2">@gmail.com</span>
       </div>
-      
+
       <div class="input-group mb-3">
         <span class="input-group-text">Личные данные</span>
         <textarea name="data" class="form-control" aria-label="With textarea"></textarea>
@@ -28,10 +28,36 @@
       <div class="d-grid gap-2 col-6 mx-auto">
        <input class="btn btn-primary" type="submit">
       </div>
-        
     </div>
 </form>
+<form action="post.php" method="post">
+<?php
+$servername = "localhost:3307";
+$username = "123";
+$password = "123";
+$dbname = "users";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, surname, mail, data FROM userlog";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Имя: " . $row["name"]. " Фамилия: " . $row["surname"]. " Почта: " . $row["mail"]. "@gmail.com". " Данные: " . $row["data"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+</form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
--- post.php
+++ post.php
@@ -17,14 +17,29 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO userlog (name, surname, mail, data)
+if ($result = mysqli_query($conn, "SELECT mail FROM userlog WHERE mail = '$email'")) {
+  if (mysqli_num_rows($result) != 0) {
+    echo"Такая почта уже существует";
+     } else {
+      $sql = "INSERT INTO userlog (name, surname, mail, data)
 VALUES ('$named', '$surnamed', '$email', '$date')";
+if (strlen($named) < 3 or strlen($surnamed)<3 or strlen($email)<3){
+  echo "Некорректные данные(Имя, фамилия и почта не могут быть меньше трёх символов (наверное))";
+}
+else {
 
 if ($conn->query($sql) === TRUE) {
   echo "Проверяй БД, я всё сохранил";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
+} 
+      
+  }
+}
+
+
+
 
 $conn->close();
 ?>
